c884872cd4b86cb0ef57d78fb971cec0e581a414
Alexei <alexeitsb@gmail.com>
2016-05-12 09:57:24 -0300

    Adding user creation to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # Create admin user.
 puts "Creating admin user..."
 CASinoUser.create(authenticator: SETTINGS["authenticator"]["name"], username: "admin", name: "Administrador", password: "admin", admin: true)
+CASinoUser.create(authenticator: SETTINGS["authenticator"]["name"], username: "user", name: "Usuário", password: "user")
 puts "DONE!"
