5f2b3039782b12c754f015ed9f10d7919a33f2ee
Alexei <alexeitsb@gmail.com>
2016-05-12 10:00:16 -0300

    Changing output text

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Create admin user.
-puts "Creating admin user..."
+puts "Creating users..."
 CASinoUser.create(authenticator: SETTINGS["authenticator"]["name"], username: "admin", name: "Administrador", password: "admin", admin: true)
 CASinoUser.create(authenticator: SETTINGS["authenticator"]["name"], username: "user", name: "Usuário", password: "user")
 puts "DONE!"
